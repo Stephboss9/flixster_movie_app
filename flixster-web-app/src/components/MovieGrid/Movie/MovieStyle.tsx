e2905de64ae46153b56a5e83268292acd5792b2d
Stephboss9 <mbengastephane34@gmail.com>
2024-01-31 00:40:41 -0500

    added reveal effect animation to correct component

--- flixster-web-app/src/components/MovieGrid/Movie/MovieStyle.tsx
+++ flixster-web-app/src/components/MovieGrid/Movie/MovieStyle.tsx
@@ -1,22 +1,5 @@
 import { styled, keyframes } from "styled-components";
 
-export const MovieCardWrapper = styled.div`
-    flex: 0 1 4%;
-    text-align:center;
-    display:flex;
-    flex-direction:column;
-    align-items:center;
-    margin-bottom: 40px;
-    color: rgb(252, 254, 255);
-    &:hover {
-        z-index:1;
-    }
-    position: relative;
-`;
-export const MovieTitle = styled.p`
-    color:inherit;
-    width:100%;
-    `;
 
 const revealAnimation = keyframes`
     0% {
@@ -30,14 +13,32 @@ const revealAnimation = keyframes`
     }
 `;
 
+export const MovieCardWrapper = styled.div`
+    flex: 0 1 4%;
+    text-align:center;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    margin-bottom: 40px;
+    color: rgb(252, 254, 255);
+    &:hover {
+        z-index:1;
+    }
+    position: relative;
+    animation: ${revealAnimation} .8s ease-in-out;
+`;
+export const MovieTitle = styled.p`
+    color:inherit;
+    width:100%;
+    `;
+
 export const MoviePoster = styled.img`
     border: 1px solid #333;
     transition: transform 400ms ease-in-out;
     box-shadow: var(--movie-card-bs);
     margin-bottom: 18px;
     cursor:pointer;
-    animation: ${revealAnimation} 1.2s ease-in-out;
-    &:hover {
+     &:hover {
         border: 1px solid white;
         transform: scale(1.07);
     }
