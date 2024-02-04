81e3b158c222853d8281ed07412438127bcab438
stephane mbenga <mbengastephane34@gmail.com>
2024-02-03 20:35:14 -0500

    Updated navigation component tests

--- flixster-web-app/src/test/Navigation.test.tsx
+++ flixster-web-app/src/test/Navigation.test.tsx
@@ -3,22 +3,29 @@ import { render, screen} from "@testing-library/react"
 import Navigation from "../components/Navigation/Navigation";
 
 describe ('Navigation Component', () => {
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
   test('Navigation component renders', () => {
      // mocks states in navigation component
      const setStateMock = vi.fn();
+     const setMoviesMock = vi.fn();
+     const setPageMock = vi.fn();
 
      // eslint-disable-next-line @typescript-eslint/no-explicit-any
      const useStateMock: any = (useState: any) => [useState, setStateMock];
-     vi.spyOn(React, 'useState').mockImplementation(useStateMock);
+     vi.spyOn(React, 'useState').mockImplementationOnce(useStateMock);
      
-    render(<Navigation/>);
-
+    render(<Navigation setMovies={setMoviesMock} page={1} setPage={setPageMock}/>);
+    
     // checks existence of title
     expect(screen.getByTestId('title')).toBeInTheDocument();
     expect(screen.getByTestId('title')).toHaveTextContent("Flixster");
 
     // checks existence of Nav Links
-    expect(screen.getAllByTestId('nav-link').length).toBe(5);
+    expect(screen.getAllByTestId('nav-link').length).toBe(4);
 
     // checks existence of search button
     expect(screen.getByTestId('search-button')).toBeInTheDocument();
@@ -29,5 +36,4 @@ describe ('Navigation Component', () => {
     // checks existence of search input
     expect(screen.getByTestId('search-input')).toBeInTheDocument();
     });
-
 });
