acaf6f0a84056487fc5550d3391ae5ad545de1dc
Stephboss9 <mbengastephane34@gmail.com>
2024-03-08 13:45:38 -0500

    updated prop type for movie component

--- flixster-web-app/src/components/MovieGrid/Movie/Movie.tsx
+++ flixster-web-app/src/components/MovieGrid/Movie/Movie.tsx
@@ -1,4 +1,5 @@
 import starIcon from '../../../assets/star_icon.png';
+
 import {
     MovieCardWrapper,
     MoviePoster,
@@ -8,19 +9,28 @@ import {
 } from './MovieStyle';
 
 type MovieCardProps = {
+    movieId: number;
     title: string;
     posterUrl: string;
-    rating:number;
+    rating: number;
+    overview: string;
+    releaseDate: string;
+    showMore: (movieId: number, title: string, overview: string) => void;
 };
 
 // movie card component
-const Movie = ({title, posterUrl, rating}: MovieCardProps) => {
-    const imgBaseUrl:string = import.meta.env.VITE_IMG_BASE_URL;
+const Movie = ({ movieId, title, posterUrl, rating, overview, showMore }: MovieCardProps) => {
+
+    const imgBaseUrl: string = import.meta.env.VITE_IMG_BASE_URL;
     return (<>
-        <MovieCardWrapper>
-            <MoviePoster src={imgBaseUrl + posterUrl}/>
+        <MovieCardWrapper
+            onClick={() => {
+                showMore(movieId, title, overview);
+            }
+            }>
+            <MoviePoster src={imgBaseUrl + posterUrl} />
             <MovieRating>
-                <RatingIcon src={starIcon}/> 
+                <RatingIcon src={starIcon} />
                 {` ${(Math.round(rating))}/10`}
             </MovieRating>
             <MovieTitle>{title}</MovieTitle>
