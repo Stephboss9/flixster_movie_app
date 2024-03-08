277cd214cadefb23638b5f3d6f69298e784c8dc7
Stephboss9 <mbengastephane34@gmail.com>
2024-03-08 01:33:32 -0500

    added movie-pop up with movie info and trailer

--- flixster-web-app/src/components/MovieGrid/MovieGrid.tsx
+++ flixster-web-app/src/components/MovieGrid/MovieGrid.tsx
@@ -1,26 +1,67 @@
 import {
-    MovieGridWrapper
+    MovieGridWrapper,
+    Modal,
+    ModalContent,
+    Trailer,
+    MovieInfo,
+    MovieTitle,
+    MovieDescription,
 } from './MovieGridStyle'
 import Movie from './Movie/Movie'
 import { MovieType } from '../../types/types'
+import { useState } from 'react';
+import ApiClient from '../../../services/api-client';
+
 
 type MovieGridProps = {
     movies: Array<MovieType>;
+    apiClient: ApiClient;
 }
 
-const MovieGrid = ({ movies }: MovieGridProps) => {
+const MovieGrid = ({ movies, apiClient }: MovieGridProps) => {
+    const [open, setOpen] = useState(false);
+    const [movieInfo, setMovieInfo] = useState({ overview: '', releaseDate: '', videoLink: '', rating: 0, title: '', time: 0 });
+
+    const handleOpen = async (movieId: number) => {
+        // gather info for movie that was clicked on
+        const movieInfo = (await apiClient.getMovieInfo(movieId)).data;
+        const movieVid = (await apiClient.getMovieVideo(movieId)).data;
+        const movieVidLink: string = movieVid[0].key;
+        setMovieInfo({
+            overview: movieInfo.overview, releaseDate: movieInfo.release_date,
+            videoLink: movieVidLink, rating: movieInfo.vote_average, time: movieInfo.runtime, title: movieInfo.title,
+        });
+
+        setOpen(true);
+    }
+    const handleClose = () => setOpen(false);
+
     return (<>
         <MovieGridWrapper data-testid="movie-grid">
             {movies?.map(movie =>
-                <Movie key={movie.id} title={movie.title} 
-                       posterUrl={movie.poster_path} rating={movie.vote_average} 
-                       data-testid="movie"
+                <Movie key={movie.id} movieId={movie.id} title={movie.title} showMore={handleOpen}
+                    posterUrl={movie.poster_path} rating={movie.vote_average}
+                    data-testid="movie"
+
                 />
             )};
+            <Modal
+                open={open}
+                onClick={handleClose}
+                aria-labelledby="unstyled-mod"
+                aria-describedby="unstyled-modal-description">
+                <ModalContent>
+                    <Trailer src={movieInfo.videoLink}></Trailer>
+                    <MovieTitle>{movieInfo.title}</MovieTitle>
+                    <MovieInfo>{movieInfo.time + " min | " + Math.round(movieInfo.rating) + "/10 | released: " + movieInfo.releaseDate}</MovieInfo>
+                    <MovieDescription>{movieInfo.overview}</MovieDescription>
+                </ModalContent>
+            </Modal>
         </MovieGridWrapper>
-
     </>);
 }
 
+
 export default MovieGrid;
+
 
--- flixster-web-app/src/components/MovieGrid/MovieGridStyle.tsx
+++ flixster-web-app/src/components/MovieGrid/MovieGridStyle.tsx
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import { Modal as BaseModal } from '@mui/base/Modal';
 
 export const MovieGridWrapper = styled.div`
     display:flex;
@@ -6,7 +7,72 @@ export const MovieGridWrapper = styled.div`
     align-items:center;
     flex-wrap:wrap;
     width:100%;
+    position:relative;
+    top:80px;
     padding: 30px 0px;
     font-family:Staatliches;
     font-size: x-large;
 `;
+
+export const Modal = styled(BaseModal)`
+  position: fixed;
+  z-index: 1300;
+  inset: 0;
+  display: flex;
+  flex-direction:column;
+  background:radial-gradient(black, transparent);
+  align-items: center;
+  justify-content: center;
+`
+
+const animateTop = keyframes`
+    0% {
+        top:-300px;
+        opacity:0;
+    }
+    100% {
+        top:0; 
+        opacity:1;
+    }
+`;
+
+export const ModalContent = styled.div`
+    display: flex;
+    position:relative;
+    flex-direction: column;
+    text-align:left;
+    align-items: center;
+    font-family: 'Kanit', sans-serif;
+    height:600px;
+    width:50%;
+    border: 1px solid white;
+    background: var(--background-color);
+    color: rgb(255, 255, 255);
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    animation:${animateTop} 1s ease;
+
+`
+
+export const TrailerWrapper = styled.div`
+  
+`
+export const Trailer = styled.iframe.attrs(props => ({
+    src: `https://www.youtube.com/embed/${props.src}`,
+    title: 'Youtube Video Player',
+    frameBorder: 0,
+    allow: 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture',
+    allowFullScreen: true,
+}))`
+width:100%;
+height:390px;
+`
+
+export const MovieInfo = styled.div`
+    display: flex;
+    justify-content:space-evenly;
+    
+`
+export const MovieTitle = styled.h1`
+`;
+export const MovieDescription = styled.p`
+width:70%`;
