190b3283254dbf1490d5d3546a1646aa04d1fa8e
Stephboss9 <mbengastephane34@gmail.com>
2024-03-21 23:16:06 -0400

    created new state to keep track of current movie list (rending, upcoming), etc

--- flixster-web-app/src/App.tsx
+++ flixster-web-app/src/App.tsx
@@ -1,28 +1,32 @@
 import Navigation from './components/Navigation/Navigation';
 import { useState, useEffect } from 'react';
 import MovieGrid from './components/MovieGrid/MovieGrid';
+import { useMovies } from '../src/hooks/useMovies';
 import ApiClient from '../services/api-client';
+import { MovieType } from './types';
 
 const App = () => {
 
-  const [movies, setMovies] = useState<[]>([]);
-  const [page, setPage] = useState(1);
-  const apiClient = new ApiClient();
+  const [movies, setMovies] = useState<Array<MovieType>>([]);
+  const [page, setPage] = useState<number>(1);
+  const [movieListCategory, setMovieListCategory] = useState<string>('now_playing');
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const api: ApiClient = new ApiClient();
+  const {
+    isLoading,
+    isError,
+    error,
+    hasNextPage,
+  } = useMovies({ page, movieListCategory, api, searchQuery, movies, setMovies });
 
 
 
-  useEffect(() => {
-    const getNowPlaying = async () => {
-      const { data } = await apiClient.getMovies(page, 'now_playing');
-      setMovies(data);
-    }
-    getNowPlaying();
-  }, [])
+
 
   return (
     <>
-      <Navigation setMovies={setMovies} page={page} setPage={setPage} />
-      <MovieGrid movies={movies} apiClient={apiClient} />
+      <Navigation setMovieListCategory={setMovieListCategory} setMovies={setMovies} page={page} setPage={setPage} />
+      <MovieGrid movies={movies} apiClient={api} isLoading={isLoading} hasNextPage={hasNextPage} setPage={setPage} />
     </>
   );
 }
