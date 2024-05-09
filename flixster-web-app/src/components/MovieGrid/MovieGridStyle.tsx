e16ec15a709cfda43f42a8b223522614dab0d107
stephane mbenga <mbengastephane34@gmail.com>
2024-05-09 14:41:55 -0400

    made movie info modal responsive

--- flixster-web-app/src/components/MovieGrid/MovieGridStyle.tsx
+++ flixster-web-app/src/components/MovieGrid/MovieGridStyle.tsx
@@ -45,6 +45,9 @@ export const ModalContent = styled.div`
     font-family: 'Kanit', sans-serif;
     height:600px;
     width:50%;
+    @media screen and (max-width: 900px) {
+        width: 80%;
+    }
     background: var(--background-color);
     color: rgb(255, 255, 255);
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
